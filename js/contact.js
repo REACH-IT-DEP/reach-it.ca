d16dffb5a0ea7158628ea71c9cf48e4c02435868
The3Null4Player613310 <the.3null.4player.6.13.3.10.@gmail.com>
2026-01-25 06:58:39 +0000

    UPDATE

--- js/contact.js
+++ js/contact.js
@@ -7,7 +7,7 @@ function formContact()
     let emailAddress=document.getElementById('content-form-contact-email');
     let emailSubject=document.getElementById('content-form-contact-subject');
     let emailBody=document.getElementById('content-form-contact-body');
-    window.location.href = 'mailto:'+ email + '?subject=' + encodeURIComponent(emailSubject) + '&body=' + encodeURIComponent(emailBody);
+    window.location.href = 'mailto:'+ emailAddress + '?subject=' + encodeURIComponent(emailSubject) + '&body=' + encodeURIComponent(emailBody);
   }
 
   document.getElementById('content-form-contact').addEventListener('submit', formContactSubmit);
